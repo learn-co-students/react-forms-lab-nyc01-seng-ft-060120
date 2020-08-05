0f7b9beeed4d9249b38bb505208fb79e82a6b13c
edensongjbs <edensongjbs@gmail.com>
2020-08-05 16:50:53 -0400

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -5,21 +5,33 @@ class LoginForm extends React.Component {
     super();
 
     this.state = {};
+    this.state.password=""
+    this.state.username=""
+  }
+
+  changeHandler = (e) => {
+    this.setState({[e.target.name]: e.target.value})
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={(e) => {
+        e.preventDefault()
+        if (this.state.username.length>0 && this.state.password.length>0) {
+          this.props.handleLogin(this.state)
+        }
+        }
+      }>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input onChange={e => this.changeHandler(e)} id="username" name="username" type="text" value={this.state.username}/>
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input onChange={e => this.changeHandler(e)} id="password" name="password" type="password" value={this.state.password} />
           </label>
         </div>
         <div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -5,13 +5,23 @@ class TwitterMessage extends React.Component {
     super();
 
     this.state = {};
+    this.state.message=""
+  }
+
+  updateCount = (e) => {
+    this.setState({message: e.target.value})
+  }
+  
+  remainingChars = () => {
+    return this.props.maxChars - this.state.message.length
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input onChange={ e => this.updateCount(e) } type="text" name="message" id="message" value={this.state.message} />
+        <p>{this.remainingChars()}</p>
       </div>
     );
   }
