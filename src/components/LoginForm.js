ec6e46dd252f41c86ad248a2c36adf17da025685
Andy Agus <Redagus@gmail.com>
2020-08-06 17:27:36 -0400

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,22 +4,43 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: "",
+      password: ""
+    };
   }
+
+    usernameChangeHandler = (e) => {
+      this.setState({username: e.target.value})
+    }
+
+    passwordChangeHandler = (e) => {
+      this.setState({password: e.target.value})
+    }
+
+    submitHandler = (e) => {
+      e.preventDefault()
+      if (this.state.username && this.state.password){
+       this.props.handleLogin(e) 
+     }else{
+      null
+     }
+      
+    }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.submitHandler}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input id="username" name="username" value={this.state.username} onChange={this.usernameChangeHandler} type="text" />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input id="password" name="password" type="password" value= {this.state.password} onChange={this.passwordChangeHandler} />
           </label>
         </div>
         <div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,22 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      name:""
+    };
+  }
+
+  changeHandler = (e) =>{
+    this.setState({name: e.target.value})
   }
 
   render() {
+    let charNumber = this.props.maxChars - this.state.name.length
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" value={this.state.name} name="message" id="message" onChange={this.changeHandler}/>
+        {charNumber}
       </div>
     );
   }
